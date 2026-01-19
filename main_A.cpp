0d0be862928790e08590a49bc179fcd0d5b33d25
Abdur Rahaman <phd2401141001@iiti.ac.in>
2026-01-19 11:53:19 +0530

    Create main_A.cpp

---
+++ main_A.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+// TODO: Implement Logic for Problem A (Numerical Verification)
+
+void solve(int id) {
+    // Your code here
+    // Output exactly 3 lines: Big-O: ..., Big-Omega: ..., Theta: ...
+}
+
+int main() {
+    int T;
+    if (cin >> T) {
+        while(T--) {
+            int id;
+            cin >> id;
+            solve(id);
+        }
+    }
+    return 0;
+}
